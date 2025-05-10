7845e72247a0a018494930f31826e5aafa20cd5d
Ealise <10503@holbertonstudents.com>
2025-05-10 11:40:56 +1000

    fix

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     args = sys.argv[1:]
     count = len(args)
     if count == 0:
-        print("0 arguments")
+        print("0 arguments.")
     elif count == 1:
         print("1 argument:")
     else:
