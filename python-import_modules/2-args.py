cfb2d66f1fcef92ab34397c05b316b5ea077f442
Ealise <10503@holbertonstudents.com>
2025-05-10 11:20:00 +1000

    fix

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import sys
     args = sys.argv[1:]
-    print("{} argument{}".format(len(args), "" if len(args) == 1 else "s"))
+    print("{} argument{}:".format(len(args), "" if len(args) == 1 else "s"))
     for i, args in enumerate(args, start=1):
         print("{}: {}".format(i, args))
