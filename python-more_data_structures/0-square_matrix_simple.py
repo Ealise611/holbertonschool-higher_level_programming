5611fec1db6d83e65b62f0cef725dc05079cddfd
Ealise <10503@holbertonstudents.com>
2025-05-13 11:19:54 +1000

    fix length

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    square_matrix = list(map(lambda row:
-                             list(map(lambda x: x * x, row)), matrix))
+    square_matrix = list(
+        map(lambda row: list(map(lambda x: x * x, row)), matrix))
     return square_matrix
 
