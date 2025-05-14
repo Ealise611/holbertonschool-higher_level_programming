1d10f6512fab632410758299d90a88fbefed4206
Ealise <10503@holbertonstudents.com>
2025-05-14 12:54:36 +1000

    fix

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-                'C': 100, 'D': 500, 'M': 1000}
+            'C': 100, 'D': 500, 'M': 1000}
     result = 0
     i = 0
     if not isinstance(roman_string, str):
