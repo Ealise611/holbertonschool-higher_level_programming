c8b8db525ca8eb1bcaf50bcd054b1baf9c0a2a70
Ealise <10503@holbertonstudents.com>
2025-05-14 14:56:31 +1000

    fix

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     if value == None:
-        return None
+        return False
     try:
         print("{:d}".format(value))
         return True
