e1c24c89605cb56a4c26e152c60ecd6f0ce1d6d9
Ealise <10503@holbertonstudents.com>
2025-05-15 18:17:23 +1000

    fix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -20,26 +20,26 @@ The result is a new matrix with the results rounded to 2 decimal places.
         raise TypeError(
             "matrix must be a matrix (list of lists) of integers/floats"
         )
-    
+
     for row in matrix:
         for item in row:
             if not isinstance(item, (float, int)):
                 raise TypeError(
                     "matrix must be a matrix (list of lists) of integers/floats"
                     )
-            
+
     row_length = len(matrix[0])
     for row in matrix:
         if len(row) != row_length:
             raise TypeError(
                 "Each row of the matrix must have the same size"
                 )
-        
+
     if  not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-    
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    
+
     new_matrix = [[round(item / div, 2) for item in row] for row in matrix]
     return new_matrix
