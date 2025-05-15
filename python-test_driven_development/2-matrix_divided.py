07911451381d27bff7405374980a868238c2441a
Ealise <10503@holbertonstudents.com>
2025-05-15 17:17:36 +1000

    fix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -8,32 +8,27 @@ result rounded to two decimal.
 
 def matrix_divided(matrix, div):
     """
-    Adds two integers or floats.
+Divides all elements of a matrix.
 
-    Args:
-        a (int or float): First number.
-        b (int or float, optional): Second number. Defaults to 98.
-
-    Raises:
-        TypeError: If a or b is not an integer or float.
-        OverflowError: If float is too large.
-        ValueError: If float is NaN.
-
-    Returns:
-        int: The result of the addition.
-    """
+Each element in the matrix (a list of lists) is divided by div.
+The result is a new matrix with the results rounded to 2 decimal places.
+"""
     if not isinstance(matrix, list) or not all (isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    
     for row in matrix:
         for item in row:
             if not isinstance(item, (float, int)):
                 raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            
     row_length = len(matrix[0])
     for row in matrix:
         if len(row) != row_length:
             raise TypeError("Each row of the matrix must have the same size")
+        
     if  not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
+    
     if div == 0:
         raise ZeroDivisionError("division by zero")
     
