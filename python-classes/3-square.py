2169458c694cdbfe70942445783d41c6951f41c9
Ealise <10503@holbertonstudents.com>
2025-05-19 13:32:30 +1000

    area

---
+++ python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module defines an empty Square class
+"""
+
+
+class Square:
+    """
+    this square class
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        result = self.__size ** 2
+        return result
+    
