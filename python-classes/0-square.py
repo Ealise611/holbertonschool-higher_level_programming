dcacf0d54d0f8d790de3f83441f3f2ab15bd80f5
Ealise <10503@holbertonstudents.com>
2025-05-19 22:55:52 +1000

    fix

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """
-This module defines an empty Square class
+This module defines a simple, empty Rectangle class.
+It can be extended later to include attributes such as width and height.
 """
 
 
 class Square:
     """
-    this is an empty square
+    A class to represent a rectangle.
     """
     pass
