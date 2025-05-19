e42c7d374a15b277b00cdfa4f94f2c2f6769ff28
Ealise <10503@holbertonstudents.com>
2025-05-19 13:21:54 +1000

    square

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module defines an empty Square class
+"""
+
+
+class Square:
+    """
+    this square class
+    """
+    def __init__(self, size):
+        self.__size = size
