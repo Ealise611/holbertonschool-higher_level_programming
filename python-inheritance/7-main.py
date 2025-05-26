0ea8c153317dd259f63591440ec4d48456397edd
Ealise <10503@holbertonstudents.com>
2025-05-26 15:41:02 +1000

    test

--- python-inheritance/7-main.py
+++ python-inheritance/7-main.py
@@ -22,6 +22,6 @@ except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
 
 try:
-    bg.integer_validator("age",True)
+    bg.integer_validator("age",{3, 4})
 except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
