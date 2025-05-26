77f3a8c65bf5e762eee8cf42acc05cdc0ba3b62f
Ealise <10503@holbertonstudents.com>
2025-05-26 13:39:31 +1000

    raise exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+BaseGeometry module
+"""
+
+
+class BaseGeometry:
+    """
+    raise exception message 
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
