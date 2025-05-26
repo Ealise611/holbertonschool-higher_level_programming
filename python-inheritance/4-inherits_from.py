c54b6baa2bf576ec8f84177aac6b1cc801aebc39
Ealise <10503@holbertonstudents.com>
2025-05-26 13:19:06 +1000

    fix

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,14 +3,18 @@
 This module provides a function to determine whether an object is an instance
 of a subclass (direct or indirect) of a specified class.
 
-It does not return True for objects that are instances of the specified class itself,
+It does not return True for objects that are instances of the
+specified class itself,
 only for those that inherit from it.
 """
+
+
 def inherits_from(obj, a_class):
     """
     Determine if an object is an instance of a subclass of the specified class.
 
-    This function returns True if the object's class is a subclass (either directly
+    This function returns True if the object's class is a subclass
+    (either directly
     or indirectly) of the specified class. It returns False if the object is
     an instance of the specified class itself or of an unrelated class.
     """
