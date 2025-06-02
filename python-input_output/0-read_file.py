01ea1b061181477fabe5d0bb5a81893a89cff390
Ealise <10503@holbertonstudents.com>
2025-06-02 14:19:58 +1000

    fix

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,12 +6,14 @@ The file is opened with UTF-8 encoding and its
 contents are printed to standard output.
 If the file does not exist or cannot be opened, an exception will be raised.
 """
+
+
 def read_file(filename=""):
     """
     Reads a text file (UTF-8) and prints its content to stdout.
 
     Args:
-        filename (str): The name of the file to read. Defaults to an empty string.
+    filename (str): The name of the file to read. Defaults to an empty string.
 
     """
     with open(filename, 'r', encoding="utf-8") as f:
