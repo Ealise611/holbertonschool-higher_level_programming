3a5545346f725482cd3b34742ba05610513e4118
Ealise <10503@holbertonstudents.com>
2025-06-02 14:18:12 +1000

    fix

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
+"""
+This module provides a function to read and print the contents of a text file.
+
+The file is opened with UTF-8 encoding and its
+contents are printed to standard output.
+If the file does not exist or cannot be opened, an exception will be raised.
+"""
 def read_file(filename=""):
-    with open('my_file_0.txt', 'r', encoding="utf-8") as f:
+    """
+    Reads a text file (UTF-8) and prints its content to stdout.
+
+    Args:
+        filename (str): The name of the file to read. Defaults to an empty string.
+
+    """
+    with open(filename, 'r', encoding="utf-8") as f:
         print(f.read())
