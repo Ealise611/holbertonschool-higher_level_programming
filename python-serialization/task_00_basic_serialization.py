a6dd99d950f52b72794363546701e185327cde16
Ealise <10503@holbertonstudents.com>
2025-06-03 14:48:12 +1000

    serialisation using json

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+this module has two function
+1-serialize_and_save_to_file
+2-load_and_deserialize
+"""
+
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    """
+    this function serialize and save data to file
+    """
+    with open(filename, 'w') as f:
+        json.dump(data, f)
+
+def load_and_deserialize(filename):
+    with open(filename, 'r') as f:
+        json.load(f)
