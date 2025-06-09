6e82238315cec66af2c186265c9ab4059e274fb5
Ealise <10503@holbertonstudents.com>
2025-06-09 22:36:55 +1000

    api json

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,15 +1,21 @@
 import http.server
 import socketserver
+import json
 
 class SimpleHandler(http.server.BaseHTTPRequestHandler):
     """
     this is a simple handler class
     """
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.wfile.write(b'Hello, this is a simple API!')
+        if self.path == '/data':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+
+            data = {"name": "John", "age": 30, "city": "New York"}
+            json_data = json.dumps(data)
+            self.wfile.write(json_data.encode('utf-8'))
+        
 
 PORT = 8000
 
