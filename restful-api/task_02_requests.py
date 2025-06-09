e886856ec2d67e2c0d15e391be785ff748ba0c8c
Ealise <10503@holbertonstudents.com>
2025-06-09 16:32:39 +1000

    fix

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -25,7 +25,7 @@ def fetch_and_save_posts():
     if response.status_code == 200:
         posts = response.json()
         filtered_post = [
-            {['id'], ['title'], ['body']}
+            {'id', 'title', 'body'}
             for post in posts
         ]
     with open('posts.csv', 'w') as f:
