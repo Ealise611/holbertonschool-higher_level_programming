c431a29cdcad6b274ffd39e33e8cd407b5859141
Ealise <10503@holbertonstudents.com>
2025-06-23 19:22:19 +1000

    create first table

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- This is a script that creates a table named `first_table` in the current database in MySQL server.
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256),
+);
