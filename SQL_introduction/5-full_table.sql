d8b4973b686fc1a23a580c5950907f0d7a74a173
Ealise <10503@holbertonstudents.com>
2025-06-23 20:43:37 +1000

    show create table

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- this script prints the description of the `first_table` table in the hbtn_0c_0 database in MySQL server.
+-- It is assumed that the database `hbtn_0c_0` and the table `first_table` already exist.
+SHOW CREATE TABLE first_table;
