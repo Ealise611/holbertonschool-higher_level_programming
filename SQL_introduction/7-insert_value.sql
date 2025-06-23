c709ce4e6116fd1cf5390b054f531df1c92aa7c7
Ealise <10503@holbertonstudents.com>
2025-06-23 21:16:34 +1000

    insert new row

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- This script inserts a new row into the `first_table` in the `hbtn_0c_0` database in MySQL server.
+-- It is assumed that the database `hbtn_0c_0` and the table `first_table` already exist.
+INSERT INTO first_table (id, name) VALUES (89, Best School);
