ce20e60f8358f35fd6eb47bed92f0a6d23767178
Ealise <10503@holbertonstudents.com>
2025-06-24 13:10:51 +1000

    fix if user exists

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,6 @@
 -- This script creates the MySQL user 'user_0d_1' with password 'user_0d_1_pwd' and grants all privileges on the database 'hbtn_0d_1' to this user.
-CREATE USER 'user_0d_1'@'Localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- If the user already exists, it will not be created again.
+CREATE USER 'user_0d_1'@'Localhost' IDENTIFIED BY 'user_0d_1_pwd'
+IF NOT EXISTS;
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'Localhost';
 FLUSH PRIVILEGES;
