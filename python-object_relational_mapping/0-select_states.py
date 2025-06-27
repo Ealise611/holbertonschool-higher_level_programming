4f872a388f26e7646730d9600cafe3beddb6b622
Ealise <10503@holbertonstudents.com>
2025-06-27 21:41:28 +1000

    fix

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,19 +5,26 @@ This script connects to a MySQL database and retrieves all rows from the
 """
 
 import MySQLdb
+import sys
 
-conn = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user="root",
-    passwd="root",
-    db="hbtn_0e_0_usa",
-    charset="utf8"
-)
-cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+if __name__ == "__main__":
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user="root",
+        passwd="root",
+        db="hbtn_0e_0_usa",
+        charset="utf8"
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
