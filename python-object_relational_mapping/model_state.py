90c1b466dd2d00177532775822a89fc8b4e824d8
Ealise <10503@holbertonstudents.com>
2025-06-29 11:33:59 +1000

    fix length

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -12,12 +12,13 @@ class State(Base):
     State class:
     inherits from Base
     links to the MySQL table states
-    Class attributes: id represent a colunm of an auto-generated, unique integer,
-    can't be NULL and is a primary key
-    Class attributes: name represent a column of a string with maximum length of
-    128, can't be NULL
+    Class attributes: id represent a colunm of an auto-generated,
+    unique integer, can't be NULL and is a primary key
+    Class attributes: name represent a column of a string with maximum
+    length of 128, can't be NULL
     Must use the module sqlalchemy (import sqlalchemy)
-    This script should connect to a MYSQL server running on localhost at port 3306
+    This script should connect to a MYSQL server running on
+    localhost at port 3306
     """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
