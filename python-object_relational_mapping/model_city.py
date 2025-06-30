6adae49b49307be9f6fce1b5a3523f88a534618a
Ealise <10503@holbertonstudents.com>
2025-06-30 12:18:32 +1000

    fix length

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -14,9 +14,12 @@ class City(Base):
     City class:
     inherits from Base (imported from model_state)
     links to the MySQL table cities
-    class attribute id that represents a column of an auto-generated, unique integer, can’t be null and is a primary key
-    class attribute name that represents a column of a string of 128 characters and can’t be null
-    class attribute state_id that represents a column of an integer, can’t be null and is a foreign key to states.id
+    class attribute id that represents a column of an auto-generated,
+    unique integer, can't be null and is a primary key
+    class attribute name that represents a column of
+    a string of 128 characters and can't be null
+    class attribute state_id that represents a column of an integer,
+    can't be null and is a foreign key to states.id
     You must use the module SQLAlchemy
     """
     __tablename__ = 'cities'
