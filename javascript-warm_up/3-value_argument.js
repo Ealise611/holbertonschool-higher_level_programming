16d80b0adbf260c9759c705111fa701988bf28a1
Ealise <10503@holbertonstudents.com>
2025-07-17 15:05:17 +1000

    fix

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const arg = process.argv.slice(2)
+const arg = process.argv.slice(2);
 
 if (!arg[0]) {
-    console.log('No argument');
+  console.log('No argument');
 } else {
-    console.log(arg[0]);
+  console.log(arg[0]);
 }
