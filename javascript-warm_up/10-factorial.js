c017e3e13886c724b8f08a9ed95a22403e8384cc
Ealise <10503@holbertonstudents.com>
2025-07-18 12:11:09 +1000

    factorial

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+function fact(n) {
+    let res = 1;
+    for (let i = 1; i <= n; i++) {
+        res *= i;
+    }
+    return res
+}
+const n = process.argv[2];
+console.log(fact(n));
