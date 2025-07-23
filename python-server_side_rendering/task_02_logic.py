69651f32f5729cca3b7be5747acafeda47389e8d
Ealise <10503@holbertonstudents.com>
2025-07-23 21:07:17 +1000

    fix empty page

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -19,7 +19,8 @@ def contact():
 def items():
      with open('items.json', 'r') as file:
           data = json.load(file)
-          return render_template('items.html', items=data["items"])
+          item_list = data.get("items", [])
+          return render_template('items.html', items=item_list)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
